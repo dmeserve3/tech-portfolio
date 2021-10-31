7eeb51ba82508e22b11f910d412d62b04cb3e8bb
dmeserve3 <91029846+dmeserve3@users.noreply.github.com>
2021-10-31 19:18:38 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,6 @@ const users = [];
 const siteConfig = {
     title: 'Daniel Meserve', // Title for your website.
     tagline: 'Software programmer and developer.',
-    profilePhoto: 'img/profile.png',
     url: 'https://dmeserve3.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
